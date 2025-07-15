5216040c604c44feef2e6d7d68a7dfa573339530
aoli-al <aoli.al@hotmail.com>
2025-07-15 13:45:50 -0400

    Fix build

--- flake.nix
+++ flake.nix
@@ -13,30 +13,45 @@
   };
 
   outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default =
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          toolchain = with fenix.packages.${system}; combine [
-            minimal.cargo
-            minimal.rustc
-          ];
-        in
-        (naersk.lib.${system}.override {
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = with fenix.packages.${system}; combine [
+          minimal.cargo
+          minimal.rustc
+        ];
+      in
+      {
+        packages.default = (naersk.lib.${system}.override {
           cargo = toolchain;
           rustc = toolchain;
         }).buildPackage {
           hardeningDisable = [ "all" ];
           buildInputs = with pkgs; [
-            clang
+            llvmPackages_20.clang-unwrapped
             zlib
+            elfutils.out
           ];
           nativeBuildInputs = with pkgs; [
-            elfutils
             pkg-config
+            patchelf
           ];
           src = ./.;
         };
-    });
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            toolchain
+            llvmPackages_20.clang-unwrapped
+            zlib
+            libbpf
+            elfutils
+            pkg-config
+          ];
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.elfutils.out}/lib:${pkgs.zlib}/lib:${pkgs.libbpf}/lib:$LD_LIBRARY_PATH
+          '';
+        };
+      });
 }
 
